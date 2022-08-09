a86008257bbaa4d60ac99efc9fd224803810c1b7
aarontorresweb <aarontorresweb@gmail.com>
2022-08-08 22:10:34 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+// Component Imports
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [ inputText, setInputText] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="todo-app">
+      <header>
+        <h1>Aaron's Todo App</h1> 
       </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// JS function to handle the input.
+  const handleInput = (e) => {
+    console.log(e);
+  }
+
+const Form = () => {
+    return(
+        <form>
+            <input onChange={handleInput} type="text" className="todo-input" />
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
